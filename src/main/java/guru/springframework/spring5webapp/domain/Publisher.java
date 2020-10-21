fdf0e913c0efb6f6d09d291477016db8582043d6
Fredohm <fredwaucquez@hotmail.fr>
2020-10-21 16:17:51 +0200

    Adding Entity & Reposotory for Publisher

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,3 @@
+package guru.springframework.spring5webapp.domain;public class Publisher {
+
+}
---
+++ src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,3 @@
+package guru.springframework.spring5webapp.repositories;public class PublisherRepository {
+
+}
